a58e2519e516b7f11641109c4ed5800e99724588
19813776205 <3287667398@qq.com>
2025-11-06 18:59:37 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -3,12 +3,5 @@ const {
 } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  chainWebpack: config => {
-    config.resolve.alias
-      .set("@", resolve("src"))
-      .set("assets", resolve("src/assets"))
-      .set("components", resolve("src/components"))
-      .set("base", resolve("baseConfig"))
-      .set("public", resolve("public"));
-  },
+
 })
