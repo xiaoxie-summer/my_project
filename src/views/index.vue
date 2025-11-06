a57c45c4c3133225ca821afce1ead7190c67470c
19813776205 <3287667398@qq.com>
2025-11-06 15:10:47 +0800

    首页

---
+++ src/views/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="app">
+        <div class="bachground">
+            <div class="map">
+                <!-- <el-button>map</el-button> -->
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+ 
+}
+</script>
+<style lang="scss">
+body {
+    margin: 0 !important;
+    padding: 0 !important;
+}
+
+.map {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+}
+
+.bachground {
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    background-image: url(../assets/backgroundImg/R-C.jpg);
+    background-repeat: no-repeat;
+    background-size: 100%;
+}
+</style>
